7b22483f9c26526194ec6909afc6304af1c529cb
Tkakishak <tkakishakd@mail.ru>
2023-10-19 22:03:13 +0300

    Initial commit

---
+++ src/test/java/com/example/TelegramBotForDevPro/TelegramBotForDevProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelegramBotForDevPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForDevProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
