6b34a63de14c0a0404204f0da55a6f94631fac53
Tkakishak <tkakishakd@mail.ru>
2023-11-20 19:28:18 +0300

    20.11  changes DB

--- src/main/java/com/example/TelegramBotForDevPro/service/bot/BotMenuService.java
+++ src/main/java/com/example/TelegramBotForDevPro/service/bot/BotMenuService.java
@@ -2,15 +2,9 @@ package com.example.TelegramBotForDevPro.service.bot;
 
 import com.pengrad.telegrambot.model.BotCommand;
 
-/**
- * Формирование команд меню
- */
+
 public interface BotMenuService {
 
-    /**
-     * Стартовое меню бота
-     *
-     * @return список команд стартового меню
-     */
+
     public BotCommand[] createMainMenuCommands();
 }
