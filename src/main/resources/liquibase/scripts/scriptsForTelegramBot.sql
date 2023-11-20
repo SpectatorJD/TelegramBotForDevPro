4424b045035409e272e8b6246cedab99332be7be
Deanisgarayev <garaevdenis2019@gmail.com>
2023-11-20 23:14:28 +0500

    in progress

--- src/main/resources/liquibase/scripts/scriptsForTelegramBot.sql
+++ src/main/resources/liquibase/scripts/scriptsForTelegramBot.sql
@@ -58,6 +58,62 @@ userName text
  );
 
 
+--changeset dgaraev:alter table
 
+alter table cats
+alter column name set not null;
 
+  alter table dogs
+alter column name set not null;
+
+  alter table cat_adopters
+alter column name set not null;
+
+  alter table cat_adopters
+alter column phone set not null;
+
+  alter table dog_adopters
+alter column name set not null;
+
+  alter table dog_adopters
+alter column phone set not null;
+
+  alter table report_about_cats
+alter column photo set not null;
+
+  alter table report_about_cats
+alter column ration set not null;
+
+  alter table report_about_cats
+alter column habits set not null;
+
+  alter table report_about_cats
+alter column behavior set not null;
+
+  alter table report_about_cats
+alter column lastMessage set not null;
+
+  alter table report_about_dogs
+alter column photo set not null;
+
+  alter table report_about_dogs
+alter column ration set not null;
+
+  alter table report_about_dogs
+alter column habits set not null;
+
+  alter table report_about_dogs
+alter column behavior set not null;
+
+  alter table report_about_dogs
+alter column lastMessage set not null;
+
+  alter table volunteers
+alter column name set not null;
+
+  alter table volunteers
+alter column phone set not null;
+
+  alter table volunteers
+alter column userName set not null;
 
